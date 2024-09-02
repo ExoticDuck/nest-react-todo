d32f83e38ca4fc694c44bd37baec773e684eab6f
Nikita <marchenkov.n@inbox.ru>
2024-09-02 10:57:21 +0300

    fix

--- src/components/task/update-task.tsx
+++ src/components/task/update-task.tsx
@@ -19,61 +19,11 @@ const Card = styled.div<{ isActive: boolean }>((props) => ({
   transform: "translate(-50%, -50%)",
   flexDirection: "column",
   width: 300,
-  //   height: 500,
   display: props.isActive ? "block" : "none",
   img: {
     height: 30,
   },
 }));
-
-const Title = styled.div({
-  width: "90%",
-  height: 50,
-  borderRadius: 10,
-  display: "flex",
-  alignItems: "center",
-  padding: 10,
-  boxSizing: "border-box",
-  //   backgroundColor: colors.cyan,
-});
-const Type = styled.div<{ type: TaskType }>((props) => ({
-  width: "fit-content",
-  height: 30,
-  borderRadius: 10,
-  display: "flex",
-  alignItems: "center",
-  padding: 8,
-  boxSizing: "border-box",
-  backgroundColor: getTaskColor(props.type),
-}));
-const Executor = styled.div({
-  width: "20%",
-  height: 50,
-  borderRadius: 10,
-  display: "flex",
-  alignItems: "center",
-  padding: 10,
-  boxSizing: "border-box",
-});
-const Progress = styled.div<{ progress: number }>((props) => ({
-  width: "fit-content",
-  height: 30,
-  borderRadius: 10,
-  display: "flex",
-  alignItems: "center",
-  padding: 8,
-  boxSizing: "border-box",
-  backgroundColor: getProgressColor(props.progress),
-}));
-const Deadline = styled.div({
-  width: "20%",
-  height: 50,
-  borderRadius: 10,
-  display: "flex",
-  alignItems: "center",
-  padding: 10,
-  boxSizing: "border-box",
-});
 
 type TaskPropsType = {
   task: TaskObjType;
